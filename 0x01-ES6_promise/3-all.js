c0e01ea40a26783ad6453d49b49f2c552d802a5e
sammyovia <sammyigbinovia@gmail.com>
2023-04-19 04:35:38 +0000

    all.js file

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleSignUp() {
-  Promise.all([uploadPhoto(), createUser()]).then(
-    (results) => {
-      console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
-    }, () => {
-      console.log('Signup system offline');
-    },
-  );
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([body, fullName]) => console.log(`${body.body} ${fullName.firstName} ${fullName.lastName}`))
+    .catch(() => console.log('Signup system offline'));
 }
